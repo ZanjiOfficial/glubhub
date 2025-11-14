1fb7e28b7f49fbff57aff3373b3c0c727e1b1eaa
RayvenDK <dennis99@live.dk>
2025-11-14 09:37:10 +0100

    tilføjet user

---
+++ glubhub/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace glubhub.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        [Required, MaxLength(255)] 
+        public string Username { get; set; }
+        [Required, MaxLength(255)]
+        public string Email { get; set; }
+        [Required, MaxLength(255)]
+        public string PasswordHash { get; set; }
+        [Required]
+        public string profilePicture { get; set; }
+        [Required]
+        public DateTime Creationdate { get; } 
+        = DateTime.Now;
+        [Required]
+        public int Followers { get; set; }
+        [Required]
+        public string Following { get; set; }
+        [Required]
+        public string Grups { get; set; }
+    }
+}
