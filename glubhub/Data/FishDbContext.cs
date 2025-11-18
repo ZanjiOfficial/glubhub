2b70fa5a3875db02a6e783c1c832d6d563ed0316
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2025-11-18 09:26:51 +0100

    hihi

---
+++ glubhub/Data/FishDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace glubhub.Data
+{
+    public class FishDbContext : DbContext
+    {
+        FishDbContext(DbContextOptions<FishDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<glubhub.Models.Fish> Fish { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<glubhub.Models.Fish>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Species).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Length).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Weight).IsRequired();
+            });
+        }
+    }
+}
---
+++ glubhub/Data/PictureDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace glubhub.Data
+{
+    public class PictureDbContext : DbContext
+    {
+        PictureDbContext(DbContextOptions<PictureDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<glubhub.Models.Picture> Pictures { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<glubhub.Models.Picture>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Description).IsRequired().HasMaxLength(500);
+                entity.Property(e => e.Link).IsRequired().HasMaxLength(200);
+            });
+        }
+    }
+}
