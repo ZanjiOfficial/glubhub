2e9439e6ec9f3b66d487a83f4680c631f2e26848
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2025-11-27 20:30:15 +0100

    beskede kan nu sendes fra server til cliendside window.

--- ChatHub/MainChatHub.cs
+++ ChatHub/MainChatHub.cs
@@ -3,11 +3,10 @@ namespace ChatHub
 {
     public class MainChatHub : Hub
     {
-        public override Task OnConnectedAsync()
+        public override async Task OnConnectedAsync()
         {
             Console.WriteLine("A client connected: " + Context.ConnectionId);   
-
-            return base.OnConnectedAsync();
+            await Clients.Caller.SendAsync("ReceiveSystemMessage", "Du har forbindelse!");
         }
 
     }
