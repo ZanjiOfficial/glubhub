fbd90ec64f664eb4188325e53774b4c38fb00d84
Mikkel <mikkelskyt2002@gmail.com>
2025-12-01 19:03:49 +0100

    AddedPictureToPost

--- glubhub/Models/Picture.cs
+++ glubhub/Models/Picture.cs
@@ -3,13 +3,19 @@
     public class Picture
     {
         public string Description { get; set; }
-        public string Link { get; set; }
+        public string Link { get; set; } = "";
         public int PictureId { get; set; }
         public Picture(string description, string link, int pictureId)
         {
             Description = description;
             Link = link;
             PictureId = pictureId;
+        }
+
+        public Picture ()
+        {
+            Description = "";
+            Link = "";
         }
     }
 }
