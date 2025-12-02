3eaca5efe54f0990f43fcdbab0510615165c2725
Jakob Kelvin Jensen <jensenjakob64@gmail.com>
2025-12-02 10:17:58 +0100

    Dbcontext in use instead of asked info.

---
+++ ChatContracts/ConnectedUser.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatContracts
+{
+    public class ConnectedUser
+    {
+        public string UserId { get; set; } = default!;
+        public string Email { get; set; } = string.Empty;
+    }
+}
